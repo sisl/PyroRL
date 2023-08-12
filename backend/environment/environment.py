b4ed2d2062748318603a895b73d34da4cb25ef72
cpondoc <chris.pondoc@comcast.net>
2023-08-11 21:38:23 -0700

    Add function for taking an action

--- backend/environment/environment.py
+++ backend/environment/environment.py
@@ -119,6 +119,21 @@ class FireEnvironment:
         # Vectorize operation and return the reward
         return np.sum(np.vectorize(calculate_reward)(action_space))
 
+    '''
+    Function to take action after sampling a state
+    Note: Only take an action if it does something (i.e., not evacuating an evacuated area or
+    taking a path that isn't active)
+    '''
+    def take_action(self, action, action_space):
+        # Only take action if we're doing something
+        i, j = action[0], action[1]
+        if (i != 1 and action_space[i].remaining_time and not action_space[i].evacuating and action_space[i].available_paths[j].active):
+            action_space[i].current_path = action_space[i].available_paths[j]
+            action_space[i].evacuating = True
+            action_space[i].remaining_time = action_space[i].current_path.evacuation_time
+        return action_space
+        
+
 # [TO-DO]: placeholder to initialize the environment
 if __name__ == "__main__":
     test = FireEnvironment()
