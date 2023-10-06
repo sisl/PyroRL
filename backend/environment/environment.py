3e53c62fa90a935e0bcb70a9a8f8ad9a4e7e8116
cpondoc <chris.pondoc@comcast.net>
2023-10-06 12:35:32 +0100

    Update new constructor to be more generalizable

--- backend/environment/environment.py
+++ backend/environment/environment.py
@@ -6,11 +6,12 @@ import math
 from os import stat
 import numpy as np
 from scipy.stats import bernoulli
+from collections import defaultdict
 
 class FireWorld:
     paths_list: list
 
-    def sample_constructor(self, n, m, populated_areas):#, populated_areas, paths):
+    def sample_constructor(self, n, m, populated_areas, paths):
         """
         Updated constructor to make environment more generalizable
         """
@@ -18,6 +19,19 @@ class FireWorld:
         self.state_space = np.zeros([5, n, m])
         pop_indices = np.ravel_multi_index(populated_areas.T, self.state_space[2].shape)
         self.state_space[2].ravel()[pop_indices] = 1
+
+        # Update list of paths and maintain number of paths for each area
+        self.paths_list = paths
+        path_nums = defaultdict(int)
+        def update_paths(path):
+            for p in path:
+                path_nums[tuple(p)] += 1
+        np.vectorize(update_paths)(self.paths_list)
+
+        # Update state space to reflect new paths
+        path_nums = (np.array(list(path_nums.items()), dtype=object))
+        rows, cols = zip(*path_nums[:, 0])
+        self.state_space[4][rows, cols] = path_nums[:, 1]
 
     # order of state space is fire?, fuel, populated?, evacuating?, paths
     def __init__(self): # later we will have rows and cols that the user can enter when creating the world
@@ -45,7 +59,11 @@ class FireWorld:
 
 if __name__ == "__main__":
     test = FireWorld()
+    # Constructor
     print(test.state_space)
     print("------")
+
+    # Potential other constructor? To make code more generalizable
     populated_areas = np.array([[1,2],[4,8], [6,4], [8, 7]])
-    test.sample_constructor(10, 10, populated_areas)
+    paths = np.array([[[1,0],[1,1]], [[2,2],[3,2],[4,2],[4,1],[4,0]], [[2,9],[2,8],[3,8]], [[5,8],[6,8],[6,9]], [[7,7], [6,7], [6,8], [6,9]], [[8,6], [8,5], [9,5]], [[8,5], [9,5], [7,5],[7,4]]], dtype=object)
+    test.sample_constructor(10, 10, populated_areas, paths)
