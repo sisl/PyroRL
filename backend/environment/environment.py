c01379d5855abe165fa19d44518cb1e316edb6ba
joey-obrien <joeyob7@gmail.com>
2023-10-09 21:03:14 -0700

    completed reward and path elemination functions and added state index globals

--- backend/environment/environment.py
+++ backend/environment/environment.py
@@ -2,86 +2,100 @@
 Environment for Wildfire Spread
 '''
 import copy
+from importlib.resources import path
 import math
+from pickle import POP
 import random
 from os import stat
 import numpy as np
 from scipy.stats import bernoulli
 from collections import defaultdict
 
+from collections import namedtuple
+
+FIRE_INDEX = 0
+FUEL_INDEX = 1
+POPULATED_INDEX = 2
+EVACUATING_INDEX = 3
+PATHS_INDEX = 4
+
 class FireWorld:
     """
     We represent the world as a 5 by n by m tensor. n by m is the size of the grid world,
-    while the 5 represents each of the following: [fire, fuel, populated, evacuating, paths]
+    while the 5 represents each of the following: [fire, fuel, populated_areas, evacuating, paths]
     """
-    paths_list: list
 
-    # order of state space is fire?, fuel, populated?, evacuating?, paths
-    def __init__(self, num_rows, num_cols, populated_areas, paths, custom_placement = False, num_fire_cells = 2, custom_locations = None):
+    # order of state space is fire?, fuel, populated_areas?, evacuating?, paths
+    def __init__(self, num_rows, num_cols, populated_areas_areas, paths, custom_fire_placement = False, num_fire_cells = 2, custom_fire_locations = None):
         # Define the state space
         self.state_space = np.zeros([5, num_rows, num_cols])
 
         # initialize the fire cells
-        if custom_placement:
-            fire_rows = custom_locations[:,0]
-            fire_cols = custom_locations[:,1]
-            self.state_space[0, fire_rows, fire_cols] = 1
+        if custom_fire_placement:
+            fire_rows = custom_fire_locations[:,0]
+            fire_cols = custom_fire_locations[:,1]
+            self.state_space[FIRE_INDEX, fire_rows, fire_cols] = 1
         else: 
             for _ in range(num_fire_cells):
-                self.state_space[0, random.randint(0,9), random.randint(0,9)] = 1
+                self.state_space[FIRE_INDEX, random.randint(0,9), random.randint(0,9)] = 1
 
 
         # initialize the fuel cells
         num_values = num_rows * num_cols
-        self.state_space[1] = np.random.normal(8.5,3,num_values).reshape((num_rows,num_cols))
+        self.state_space[FUEL_INDEX] = np.random.normal(8.5,3,num_values).reshape((num_rows,num_cols))
 
-        # initialize the populated areas 
-        pop_rows = populated_areas[:,0]
-        pop_cols = populated_areas[:,1]
-        self.state_space[2, pop_rows, pop_cols] = 1
+        # initialize the populated_areas areas 
+        pop_rows = populated_areas_areas[:,0]
+        pop_cols = populated_areas_areas[:,1]
+        self.state_space[POPULATED_INDEX, pop_rows, pop_cols] = 1
 
         # initialize the paths
-        self.paths_array = paths
-        for path in self.paths_array:
+        self.paths = []
+        for path in paths: #right now paths is different from self.paths but we can change this later if needed
             path_array = np.array(path)
-            rows = path_array[:,0]
-            cols = path_array[:,1]
-            self.state_space[4,rows,cols] += 1
+            path_rows = path_array[:,0]
+            path_cols = path_array[:,1]
+            self.state_space[PATHS_INDEX,path_rows,path_cols] += 1
+
+            # each path in self.paths is a list that records what the path is and whether or not the path still exists (i.e. has
+            # not been destroyed by a fire)
+            self.paths.append([np.zeros((num_rows, num_cols)), True])
+            self.paths[-1][0][path_rows, path_cols] += 1
+        """
+        self.paths = np.zeros((paths.size, num_rows, num_cols))
+        index = 0 
+        for path in paths: #right now paths is different from self.paths but we can change this later if needed
+            path_array = np.array(path)
+            path_rows = path_array[:,0]
+            path_cols = path_array[:,1]
+            self.state_space[4,path_rows,path_cols] += 1
+
+            self.paths[index, path_rows, path_cols] += 1
+            index += 1
+        """
 
         # Note: We'll also need to be able to denote which path is associated with each pop center (probably a list of lists, but do this later)
     
-    """
+
     def remove_paths(self):
-        path_cells_on_fire = np.logical_and(self.state_space[0], self.state_space[4]) # whatever cells are one fire and are on a path will be one
-        print(self.state_space[4])
-        print("")
-        print(self.state_space[0])
-        print("")
-        print(path_cells_on_fire)
-        print("")
-        on_fire_indices = np.transpose(np.nonzero(path_cells_on_fire)).tolist()
-        # for path in self.paths_array:
-        #     if set(path).isdisjoint(set())
-    """
+        for i in range(len(self.paths)):
+            if self.paths[i][1] and np.sum(np.logical_and(self.state_space[FIRE_INDEX], self.paths[i][0])) > 0:
+                # decrement the count for the paths in the state space
+                self.state_space[PATHS_INDEX] -= self.paths[i][0]
+                # remove the path
+                self.paths[i][1] = False
 
     def get_state_utility(self):
         """
         Get the total amount of utility given a current state.
         """
-        # Get which populated areas are on fire and evacuating
+        # Get which populated_areas areas are on fire and evacuating
         reward = 0
-        populated = np.where(self.state_space[2] == 1)
-        fire = self.state_space[0][populated]
-        evacuating = self.state_space[3][populated]
+        populated_areas = np.where(self.state_space[POPULATED_INDEX] == 1)
+        fire = self.state_space[FIRE_INDEX][populated_areas]
+        evacuating = self.state_space[EVACUATING_INDEX][populated_areas]
 
         # Update reward
         reward -= 100 * fire.sum()
         reward += len((np.where(fire + evacuating == 0))[0])
         return reward
-        
-
-if __name__ == "__main__":
-    populated_areas = np.array([[1,2],[4,8], [6,4], [8, 7]])
-    paths_array = np.array([[[1,0],[1,1]], [[2,2],[3,2],[4,2],[4,1],[4,0]], [[2,9],[2,8],[3,8]], [[5,8],[6,8],[6,9]], [[7,7], [6,7], [6,8], [6,9]], [[8,6], [8,5], [9,5]], [[8,5], [9,5], [7,5],[7,4]]], dtype=object)
-    test = FireWorld(10,10,populated_areas,paths_array)
-    test.remove_paths()
--- backend/environment_test.py
+++ backend/environment_test.py
@@ -4,19 +4,26 @@ Unit tests for each of the functions in environment.py
 from environment.environment import *
 import numpy as np
 
+"""
 def dummy_environment():
-    """
+    
     Set up environment for the grid world
-    """
+    
     # Define hardcoded world variables
     populated_areas = np.array([[1,2],[4,8], [6,4], [8, 7]])
     paths = np.array([[[1,0],[1,1]], [[2,2],[3,2],[4,2],[4,1],[4,0]], [[2,9],[2,8],[3,8]], [[5,8],[6,8],[6,9]], [[7,7], [6,7], [6,8], [6,9]], [[8,6], [8,5], [9,5]], [[8,5], [9,5], [7,5],[7,4]]], dtype=object)
 
     # Initialize fire world
-    test_world = FireWorld()
-    test_world.sample_constructor(10, 10, populated_areas, paths)
+    test_world = FireWorld(10,10,populated_areas,paths)
     return test_world
+"""
 
-def test_world():
-    test_world = dummy_environment()
+
+if __name__ == "__main__":
+    populated_areas = np.array([[1,2],[4,8], [6,4], [8, 7]])
+    paths_array = np.array([[[1,0],[1,1]], [[2,2],[3,2],[4,2],[4,1],[4,0]], [[2,9],[2,8],[3,8]], [[5,8],[6,8],[6,9]], [[7,7], [6,7], [6,8], [6,9]], [[8,6], [8,5], [9,5]], [[8,5], [9,5], [7,5],[7,4]]], dtype=object)
+    test_world = FireWorld(10,10,populated_areas,paths_array)
     test_world.get_state_utility()
+    # test_world.remove_paths()
+    
+    
