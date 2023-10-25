3f16fc16dba8e4a55ea9507cac0dd74988e75201
cpondoc <chris.pondoc@comcast.net>
2023-10-25 01:01:45 +0100

    Create a package !

--- wildfire_evac/setup.py
+++ wildfire_evac/setup.py
@@ -3,5 +3,13 @@ from setuptools import setup
 setup(
     name="wildfire_evac",
     version="0.0.1",
+    author='Celtics Big 3',
+    author_email='clpondoc@stanford.edu',
+    description='An RL OpenAI Gym Environment for Wildfire Evacuation',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
     install_requires = ["importlib-metadata==5.0.0", "numpy==1.23.4", "pytest==7.4.0", "scipy==1.11.3", "torch==2.1.0", "gymnasium==0.29.1", "pygame==2.5.2"]
 )
